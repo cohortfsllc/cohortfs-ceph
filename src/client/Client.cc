348b120e469ed3de58798fb84c41c3530d36adad
Adam C. Emerson <aemerson@linuxbox.com>
2011-05-19 12:41:03 -0400

    Fix readdirplus_r bug
    Readdirplus was implemented by giving readdir_cb a callback that could
    read only one directory entry.  However, the readdir_cb function kept
    calling it repeatedly.

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -4415,6 +4415,8 @@ int Client::_readdir_cache_cb(dir_result_t *dirp, add_dirent_cb_t cb, void *p)
 
     prev_name = dn->name;
     dirp->offset = next_off;
+    if (r > 0)
+      return r;
   }
 
   ldout(cct, 10) << "_readdir_cache_cb " << dirp << " on " << dirp->inode->ino << " at end" << dendl;
@@ -4459,6 +4461,8 @@ int Client::readdir_r_cb(dir_result_t *d, add_dirent_cb_t cb, void *p)
 
     dirp->offset = next_off;
     off = next_off;
+    if (r > 0)
+      return r;
   }
   if (dirp->offset == 1) {
     ldout(cct, 15) << " including .." << dendl;
@@ -4474,6 +4478,8 @@ int Client::readdir_r_cb(dir_result_t *d, add_dirent_cb_t cb, void *p)
 
     dirp->offset = 2;
     off = 2;
+    if (r > 0)
+      return r;
   }
 
   // can we read from our cache?
@@ -4525,6 +4531,8 @@ int Client::readdir_r_cb(dir_result_t *d, add_dirent_cb_t cb, void *p)
       
       off++;
       dirp->offset = pos + 1;
+      if (r > 0)
+	return r;
     }
 
     if (dirp->last_name.length()) {
@@ -4594,7 +4602,7 @@ static int _readdir_single_dirent_cb(void *p, struct dirent *de, struct stat *st
   if (c->stmask)
     *c->stmask = stmask;
   c->full = true;
-  return 0;  
+  return 1;  
 }
 
 struct dirent *Client::readdir(dir_result_t *d)
