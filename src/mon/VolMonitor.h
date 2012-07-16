60ab6507eef9e0b68038c9160d8658ff20e27f5a
Eric I <eric@linuxbox.com>
2012-07-16 11:53:25 -0400

    Fixed INCORRECT copyright and license headers in source files and added entries to COPYING file.

--- src/mon/VolMonitor.h
+++ src/mon/VolMonitor.h
@@ -3,12 +3,12 @@
 /*
  * Ceph - scalable distributed file system
  *
- * Copyright (C) 2012 
+ * Copyright (C) 2012, CohortFS, LLC <info@cohortfs.com>
+ * All rights reserved.
  *
- * This is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License version 2.1, as published by the Free Software 
- * Foundation.  See file COPYING.
+ * This file is licensed under what is commonly known as the New BSD
+ * License (or the Modified BSD License, or the 3-Clause BSD
+ * License). See file COPYING.
  * 
  */
  
