ae770f88c2e7443fc6dfc617c7012d4be8b674fa
Matt Benjamin <matt@linuxbox.com>
2013-04-24 10:05:34 -0700

    Implement reservation_state_t::remove_rsv.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/mds/Reservation.cc
+++ src/mds/Reservation.cc
@@ -24,22 +24,21 @@ bool reservation_state_t::add_rsv(ceph_reservation& rsv)
 
 bool reservation_state_t::remove_rsv(ceph_reservation& rsv)
 {
-    
+    // rsv.client and source client are confirmed eq
+    if (reservations.erase(rsv)) {
 
-    pair<set<ceph_reservation, rsv_key_cmp>::iterator,bool> ret;
+        reservations_id.erase(rsv.id);
+        reservations_client.erase(rsv.client);
 
-    ret = reservations.insert(rsv);
-    if (! ret.second)
-        return (false);
+        // fixup max_id
+        uint64_t hkey = (reservations_id.size() == 0) ? 0 :
+            reservations_id.rbegin()->first;
 
-    ceph_reservation& nrsv = const_cast<ceph_reservation&>(*(ret.first));
-    nrsv.id = ++max_id;
+        if (max_id > hkey)
+            max_id = hkey;
 
-    // populate lookup tables
-    reservations_id.insert(pair<uint64_t,ceph_reservation>(rsv.id, rsv));
-    reservations_client.insert(pair<uint64_t,ceph_reservation>(rsv.client,rsv));
+        return (true);
+    }
 
-    rsv = nrsv;
-
-    return (true);
+    return (false);
 }
