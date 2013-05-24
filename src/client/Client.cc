76e19764299dabda440d03e3514e901d0e9e934b
Adam C. Emerson <aemerson@linuxbox.com>
2013-05-23 19:17:43 -0700

    client/Client: return 1 from readdir cb on full
    This appears to save us a bit of extra work trying to fill up the buffer
    further than it can be filled.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -5131,6 +5131,8 @@ int Client::_readdir_cache_cb(dir_result_t *dirp, add_dirent_cb_t cb, void *p)
 
     prev_name = dn->name;
     dirp->offset = next_off;
+    if (r > 0)
+      return r;
   }
 
   ldout(cct, 10) << "_readdir_cache_cb " << dirp << " on " << dirp->inode->ino << " at end" << dendl;
@@ -5179,6 +5181,8 @@ int Client::readdir_r_cb(dir_result_t *d, add_dirent_cb_t cb, void *p)
 
     dirp->offset = next_off;
     off = next_off;
+    if (r > 0)
+      return r;
   }
   if (dirp->offset == 1) {
     ldout(cct, 15) << " including .." << dendl;
@@ -5201,6 +5205,8 @@ int Client::readdir_r_cb(dir_result_t *d, add_dirent_cb_t cb, void *p)
 
     dirp->offset = 2;
     off = 2;
+    if (r > 0)
+      return r;
   }
 
   // can we read from our cache?
@@ -5257,6 +5263,8 @@ int Client::readdir_r_cb(dir_result_t *d, add_dirent_cb_t cb, void *p)
       
       off++;
       dirp->offset++;
+      if (r > 0)
+	return r;
     }
 
     if (dirp->last_name.length()) {
@@ -5326,7 +5334,7 @@ static int _readdir_single_dirent_cb(void *p, struct dirent *de, struct stat *st
   if (c->stmask)
     *c->stmask = stmask;
   c->full = true;
-  return 0;  
+  return 1;
 }
 
 struct dirent *Client::readdir(dir_result_t *d)
