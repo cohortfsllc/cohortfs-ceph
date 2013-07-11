fd3e34cd5ccc8734658a8db60ebd4be918559f90
Casey Bodley <casey@linuxbox.com>
2013-07-11 15:47:47 -0400

    mds: open_remote_ino uses inode/stripe placement

--- src/mds/MDCache.h
+++ src/mds/MDCache.h
@@ -679,11 +679,7 @@ public:
   void open_remote_dirstripe(CInode *diri, stripeid_t stripeid, Context *fin);
   void open_remote_dirfrag(CStripe *stripe, frag_t fg, Context *fin);
   CInode *get_dentry_inode(CDentry *dn, MDRequest *mdr, bool projected=false);
-  void open_remote_ino(inodeno_t ino, Context *fin, bool want_xlocked=false,
-		       inodeno_t hadino=0, version_t hadv=0);
-  void open_remote_ino_2(inodeno_t ino,
-			 vector<Anchor>& anchortrace, bool want_xlocked,
-			 inodeno_t hadino, version_t hadv, Context *onfinish);
+  void open_remote_ino(inodeno_t ino, Context *fin, bool want_xlocked=false);
 
   bool parallel_fetch(map<inodeno_t,filepath>& pathmap, set<inodeno_t>& missing);
   bool parallel_fetch_traverse_dir(inodeno_t ino, filepath& path, 
