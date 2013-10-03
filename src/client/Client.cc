6e5e70bf43cedbc0b76b34755aa42cda8ee151d6
Casey Bodley <casey@linuxbox.com>
2013-10-03 11:22:13 -0400

    mds: more for cap release handling

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -2841,6 +2841,7 @@ void Client::remove_cap(Cap *cap)
     session->release = new MClientCapRelease;
   ceph_mds_cap_item i;
   i.ino = parent->ino;
+  i.stripeid = CEPH_CAP_OBJECT_INODE;
   i.cap_id = cap->cap_id;
   i.seq = cap->issue_seq;
   i.migrate_seq = cap->mseq;
@@ -3323,6 +3324,7 @@ void Client::handle_caps(MClientCaps *m)
 	session->release = new MClientCapRelease;
       ceph_mds_cap_item i;
       i.ino = m->get_ino();
+      i.stripeid = m->get_stripeid();
       i.cap_id = m->get_cap_id();
       i.seq = m->get_seq();
       i.migrate_seq = m->get_mseq();
