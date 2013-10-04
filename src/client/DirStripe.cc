9de812fd2953180c5cc7d5326e8b6b12c420d3ff
Casey Bodley <casey@linuxbox.com>
2013-10-04 13:59:37 -0400

    client: DirStripe implements CapObject

--- src/client/DirStripe.cc
+++ src/client/DirStripe.cc
@@ -3,6 +3,7 @@
 
 #include "DirStripe.h"
 #include "Inode.h"
+#include "messages/MClientCaps.h"
 
 ostream& operator<<(ostream &out, DirStripe &stripe)
 {
@@ -13,8 +14,21 @@ ostream& operator<<(ostream &out, DirStripe &stripe)
 }
 
 DirStripe::DirStripe(Inode *in, stripeid_t stripeid)
-  : parent_inode(in), ds(in->ino, stripeid),
+  : CapObject(in->cct, in->vino()),
+    parent_inode(in), ds(in->ino, stripeid), version(0),
     release_count(0), max_offset(2)
 {
+}
+
+void DirStripe::fill_caps(const Cap *cap, MClientCaps *m, int mask)
+{
+  m->stripe.nfiles = fragstat.nfiles;
+  m->stripe.nsubdirs = fragstat.nsubdirs;
+  fragstat.mtime.encode_timeval(&m->stripe.mtime);
+
+  m->stripe.rbytes = rstat.rbytes;
+  m->stripe.rfiles = rstat.rfiles;
+  m->stripe.rsubdirs = rstat.rsubdirs;
+  rstat.rctime.encode_timeval(&m->stripe.rctime);
 }
 
