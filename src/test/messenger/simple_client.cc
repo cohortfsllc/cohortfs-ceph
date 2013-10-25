7f7ad2b5f25708ff6d170ea1bac5ad4c04139f35
Matt Benjamin <matt@linuxbox.com>
2013-10-25 03:24:15 -0400

    Add test/messenger. (REBASEME)
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

---
+++ src/test/messenger/simple_client.cc
@@ -0,0 +1,45 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2013 CohortFS, LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#include <sys/types.h>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#include "common/config.h"
+#include "msg/Messenger.h"
+
+#include "common/Timer.h"
+#include "common/ceph_argparse.h"
+
+#include "global/global_init.h"
+#include "global/signal_handler.h"
+
+
+#define dout_subsys ceph_subsys_simple_client
+
+int main(int argc, const char **argv)
+{
+	vector<const char*> args;
+	argv_to_vec(argc, argv, args);
+	env_to_vec(args);
+
+	global_init(NULL, args, CEPH_ENTITY_TYPE_ANY, CODE_ENVIRONMENT_UTILITY, 0);
+
+	// do stuff
+
+	return 0;
+}
