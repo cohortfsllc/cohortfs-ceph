695fbc96083cf5e84488ae052985d7cecfe285c9
Matt Benjamin <matt@linuxbox.com>
2014-05-30 12:25:12 -0400

    Switch to threaded model, cleanups.
    The cds_filestore_dat object now handles static and dynamic
    initialization.  One backing store is created for use by all
    threads.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/test/fio_cds_filestore.cc
+++ src/test/fio_cds_filestore.cc
@@ -6,78 +6,174 @@
  *
  */
 
+#include <iostream>
+
 #include "os/ObjectStore.h"
 #include "global/global_init.h"
 
+extern "C" {
 #include <fio.h>
+#include <log.h>
+#include <pthread.h>
+}
 
 #include <cds/init.h>  //cds::Initialize и cds::Terminate
 #include <cds/gc/hp.h> //cds::gc::HP (Hazard Pointer)
 #include <cds/intrusive/skip_list_hp.h> //cds intrusive skip lists
 
-struct fio_ceph_filestore_iou {
+struct fio_cds_filestore_iou {
 	struct io_u *io_u;
 	int io_complete;
 };
 
-struct ceph_filestore_data {
-	struct io_u **aio_events;
-	ObjectStore *fs;
-};
-
-struct ceph_filestore_options {
+struct cds_filestore_options {
 	struct thread_data *td;
 	char *objectstore;
 	char *filestore_debug;
 	char *filestore_journal;
 };
 
-class CDS_Init {
-private:
+struct cds_filestore_data {
+    struct io_u **aio_events;
+    ObjectStore *fs;
     cds::gc::HP hpGC;
-public:
-    CDS_Init() : hpGC(67) // magic number for SkipListSet HP impl
-	{
-	    cds::Initialize(0);
-	    cds::threading::Manager::init();
+
+    pthread_mutex_t mtx;
+    bool setup;
+    bool initialized;
+    int nthreads;
+
+    cds_filestore_data()
+	: aio_events(NULL),
+	  fs(NULL),
+	  hpGC(167), // magic number for SkipListSet HP impl + 100
+	  mtx(PTHREAD_MUTEX_INITIALIZER),
+	  setup(false),
+	  initialized(false),
+	  nthreads(0) {
+	std::cout << "fio_cds_filestore (initialize)" << std::endl;
+	cds::Initialize(0);
+	cds::threading::Manager::init();
+    }
+
+    int cond_setup(struct thread_data *td) {
+	if (td->io_ops->data)
+	    return 0;
+	pthread_mutex_lock(&mtx);
+	if (! setup) {
+	    setup = true; // once
+	    aio_events = (struct io_u **)
+		malloc(td->o.iodepth * sizeof(struct io_u *));
+	    memset(aio_events, 0, td->o.iodepth * sizeof(struct io_u *));
+	}
+	pthread_mutex_unlock(&mtx);
+	return (0);
+    }
+
+    int thread_init(struct thread_data *td) {
+	struct cds_filestore_options *o =
+	    (struct cds_filestore_options *) td->eo;
+	ObjectStore::Transaction ft;
+	int r = 0;
+
+	pthread_mutex_lock(&mtx);
+
+	++nthreads;
+
+	if (! initialized) {
+	    vector<const char*> args;
+
+	    initialized = true; // once
+
+	    global_init(NULL, args, CEPH_ENTITY_TYPE_OSD,
+			CODE_ENVIRONMENT_UTILITY, 0);
+	    //g_conf->journal_dio = false;
+	    common_init_finish(g_ceph_context);
+	    if (o->filestore_debug) {
+		g_ceph_context->_conf->set_val("debug_filestore",
+					       o->filestore_debug);
+		g_ceph_context->_conf->apply_changes(NULL);
+	    }
+
+	    //mktemp(o->filestore_journal); // NOSPC issue
+
+	    fs =
+		ObjectStore::create(
+		    g_ceph_context,
+		    o->objectstore, td->o.directory,
+		    o->filestore_journal ? o->filestore_journal : "");
+	    if (fs == NULL) {
+		cout << "bad objectstore type " << o->objectstore << std::endl;
+		r = 1;
+		goto out;
+	    }
+	    if (fs->mkfs() < 0) {
+		cout << "mkfs failed" << std::endl;
+		r = 1;
+		goto out;
+	    }
+	    if (fs->mount() < 0) {
+		cout << "mount failed" << std::endl;
+		r = 1;
+		goto out;
+	    }
+
+	    ft.create_collection(coll_t());
+	    fs->apply_transaction(ft);
 	}
 
-    ~CDS_Init()
-	{
-	    cds::Terminate();
+    out:
+	pthread_mutex_unlock(&mtx);
+	return r;
+    }
+
+    void cleanup(struct thread_data *td) {
+	pthread_mutex_lock(&mtx);
+	int nth = --nthreads;
+	pthread_mutex_unlock(&mtx);
+	if (nth == 0) {
+	    // clean up ObjectStore
+	    fs->umount();
+	    delete fs;
+	    free(aio_events);
 	}
+    }
+
+    ~cds_filestore_data() {
+	cds::Terminate();
+    }
 };
 
-static CDS_Init cds_static_initializer;
+static cds_filestore_data cds_data[1];
 
 // initialize the options in a function because g++ reports:
 //   sorry, unimplemented: non-trivial designated initializers not supported
 static struct fio_option* init_options() {
 	static struct fio_option options[] = {{},{},{},{}};
 
-	options[0].name     = "cds_objectstore";
-	options[0].lname    = "ceph cds objectstore type";
+	options[0].name     = "objectstore";
+	options[0].lname    = "ceph objectstore type";
 	options[0].type     = FIO_OPT_STR_STORE;
 	options[0].help     = "Type of ObjectStore to create";
-	options[0].off1     = offsetof(struct ceph_filestore_options,
+	options[0].off1     = offsetof(struct cds_filestore_options,
 				       objectstore);
 	options[0].category = FIO_OPT_C_ENGINE;
 	options[0].group    = FIO_OPT_G_RBD;
 
 	options[1].name     = "filestore_debug";
-	options[1].lname    = "ceph filestore debug level";
+	options[1].lname    = "cds filestore debug level";
 	options[1].type     = FIO_OPT_STR_STORE;
-	options[1].help     = "Debug level for ceph filestore log output";
-	options[1].off1     = offsetof(struct ceph_filestore_options,
+	options[1].help     = "Debug level for cds filestore log output";
+	options[1].off1     = offsetof(struct cds_filestore_options,
 				       filestore_debug);
 	options[1].category = FIO_OPT_C_ENGINE;
 	options[1].group    = FIO_OPT_G_RBD;
 
 	options[2].name     = "filestore_journal";
-	options[2].lname    = "ceph filestore journal path";
+	options[2].lname    = "cds filestore journal path";
 	options[2].type     = FIO_OPT_STR_STORE;
 	options[2].help     = "Path for a temporary journal file";
-	options[2].off1     = offsetof(struct ceph_filestore_options,
+	options[2].off1     = offsetof(struct cds_filestore_options,
 				       filestore_journal);
 	options[2].category = FIO_OPT_C_ENGINE;
 	options[2].group    = FIO_OPT_G_RBD;
@@ -102,76 +198,43 @@ struct OnApplied : public Context {
       : io_u(io_u), t(t) {}
   void finish(int r) {
 
-	struct fio_ceph_filestore_iou *fio_ceph_filestore_iou =
-	    (struct fio_ceph_filestore_iou *)io_u->engine_data;
+	struct fio_cds_filestore_iou *fio_cds_filestore_iou =
+	    (struct fio_cds_filestore_iou *)io_u->engine_data;
 
-	fio_ceph_filestore_iou->io_complete = 1;
+	fio_cds_filestore_iou->io_complete = 1;
 
 	delete t;
   }
 };
 
-static int _fio_setup_ceph_filestore_data(
-    struct thread_data *td,
-    struct ceph_filestore_data **ceph_filestore_data_ptr)
+static struct io_u *fio_cds_filestore_event(struct thread_data *td, int event)
 {
-    struct ceph_filestore_data *ceph_filestore_data;
+    struct cds_filestore_data *cds_filestore_data =
+	(struct cds_filestore_data *) td->io_ops->data;
 
-    if (td->io_ops->data)
-	return 0;
-
-    ceph_filestore_data = (struct ceph_filestore_data*)
-	malloc(sizeof(struct ceph_filestore_data));
-    if (!ceph_filestore_data)
-	goto failed;
-
-    memset(ceph_filestore_data, 0, sizeof(struct ceph_filestore_data));
-
-    ceph_filestore_data->aio_events = (struct io_u **)
-	malloc(td->o.iodepth * sizeof(struct io_u *));
-    if (!ceph_filestore_data->aio_events)
-	goto failed;
-
-    memset(ceph_filestore_data->aio_events, 0,
-	   td->o.iodepth * sizeof(struct io_u *));
-
-    *ceph_filestore_data_ptr = ceph_filestore_data;
-
-    return 0;
-
-failed:
-    return 1;
-
+    return cds_filestore_data->aio_events[event];
 }
 
-static struct io_u *fio_ceph_filestore_event(struct thread_data *td, int event)
-{
-    struct ceph_filestore_data *ceph_filestore_data =
-	(struct ceph_filestore_data *) td->io_ops->data;
-
-    return ceph_filestore_data->aio_events[event];
-}
-
-static int fio_ceph_filestore_getevents(struct thread_data *td,
+static int fio_cds_filestore_getevents(struct thread_data *td,
 					unsigned int min,
 					unsigned int max, struct timespec *t)
 {
-    struct ceph_filestore_data *ceph_filestore_data =
-	(struct ceph_filestore_data *) td->io_ops->data;
+    struct cds_filestore_data *cds_filestore_data =
+	(struct cds_filestore_data *) td->io_ops->data;
     unsigned int events = 0;
     struct io_u *io_u;
     unsigned int i;
-    struct fio_ceph_filestore_iou *fov;
+    struct fio_cds_filestore_iou *fov;
 
     do {
 	io_u_qiter(&td->io_u_all, io_u, i) {
 	    if (!(io_u->flags & IO_U_F_FLIGHT))
 		continue;
 
-	    fov = (struct fio_ceph_filestore_iou *)io_u->engine_data;
+	    fov = (struct fio_cds_filestore_iou *)io_u->engine_data;
 	    if (fov->io_complete) {
 		fov->io_complete = 0;
-		ceph_filestore_data->aio_events[events] = io_u;
+		cds_filestore_data->aio_events[events] = io_u;
 		events++;
 	    }
 	}
@@ -185,14 +248,14 @@ static int fio_ceph_filestore_getevents(struct thread_data *td,
     return events;
 }
 
-static int fio_ceph_filestore_queue(struct thread_data *td, struct io_u *io_u)
+static int fio_cds_filestore_queue(struct thread_data *td, struct io_u *io_u)
 {
     int r = -1;
-    struct ceph_filestore_data *ceph_filestore_data =
-	(struct ceph_filestore_data *) td->io_ops->data;
+    struct cds_filestore_data *cds_filestore_data =
+	(struct cds_filestore_data *) td->io_ops->data;
     uint64_t len = io_u->xfer_buflen;
     uint64_t off = io_u->offset;
-    ObjectStore *fs = ceph_filestore_data->fs;
+    ObjectStore *fs = cds_filestore_data->fs;
     sobject_t poid(object_t(io_u->file->file_name), 0);
 
     fio_ro_check(td, io_u);
@@ -232,89 +295,47 @@ failed:
     return FIO_Q_COMPLETED;
 }
 
-static int fio_ceph_filestore_init(struct thread_data *td)
+static int fio_cds_filestore_init(struct thread_data *td)
 {
-    vector<const char*> args;
-    struct ceph_filestore_data *ceph_filestore_data =
-	(struct ceph_filestore_data *) td->io_ops->data;
-    struct ceph_filestore_options *o =
-	(struct ceph_filestore_options *) td->eo;
-    ObjectStore::Transaction ft;
-
-    global_init(NULL, args, CEPH_ENTITY_TYPE_OSD,
-		CODE_ENVIRONMENT_UTILITY, 0);
-    //g_conf->journal_dio = false;
-    common_init_finish(g_ceph_context);
-    if (o->filestore_debug) {
-	g_ceph_context->_conf->set_val("debug_filestore",
-				       o->filestore_debug);
-	g_ceph_context->_conf->apply_changes(NULL);
-    }
-
-    //mktemp(o->filestore_journal); // NOSPC issue
-
-    ObjectStore *fs =
-	ObjectStore::create(
-	    g_ceph_context,
-	    o->objectstore, td->o.directory,
-	    o->filestore_journal ? o->filestore_journal : "");
-    if (fs == NULL) {
-	cout << "bad objectstore type " << o->objectstore << std::endl;
-	return 1;
-    }
-    if (fs->mkfs() < 0) {
-	cout << "mkfs failed" << std::endl;
-	return 1;
-    }
-    if (fs->mount() < 0) {
-	cout << "mount failed" << std::endl;
-	return 1;
-    }
-
-    ft.create_collection(coll_t());
-    fs->apply_transaction(ft);
-
-    ceph_filestore_data->fs = fs;
-    return 0;
+    return cds_data->thread_init(td);
 }
 
-static void fio_ceph_filestore_cleanup(struct thread_data *td)
+static void fio_cds_filestore_cleanup(struct thread_data *td)
 {
-    struct ceph_filestore_data *ceph_filestore_data =
-	(struct ceph_filestore_data *) td->io_ops->data;
-
-    if (ceph_filestore_data) {
-	if (ceph_filestore_data->fs) {
-	    // clean up ObjectStore
-	    ceph_filestore_data->fs->umount();
-	    delete ceph_filestore_data->fs;
-	}
-	free(ceph_filestore_data->aio_events);
-	free(ceph_filestore_data);
-    }
+    cds_data->cleanup(td);
 }
 
-static int fio_ceph_filestore_setup(struct thread_data *td)
+static int fio_cds_filestore_setup(struct thread_data *td)
 {
     int r = 0;
     struct fio_file *f;
-    struct ceph_filestore_data *ceph_filestore_data = NULL;
 
-    /* allocate engine specific structure to deal with
-     * libceph_filestore. */
-    r = _fio_setup_ceph_filestore_data(td, &ceph_filestore_data);
-    if (r) {
-	log_err("fio_setup_ceph_filestore_data failed.\n");
+    /* we just don't care about subprocess operation, tell the
+     * user to add -thread */
+    if (! td->o.use_thread) {
+	r = EIO;
+	log_err("The fio_cds_filestore engine only supports threaded mode "
+		"(add -thread)\n");
+	exit(r);
 	goto cleanup;
     }
-    td->io_ops->data = ceph_filestore_data;
+
+    /* allocate engine specific structure to deal with
+     * libcds_filestore. */
+
+    r = cds_data->cond_setup(td);
+    if (r) {
+	log_err("fio_setup_cds_filestore_data failed.\n");
+	goto cleanup;
+    }
+    td->io_ops->data = cds_data;
 
     /* taken from "net" engine. Pretend we deal with files,
      * even if we do not have any ideas about files.
      * The size of the FileStore is set instead of a artificial file.
      */
     if (!td->files_index) {
-	add_file(td, td->o.filename ? : "ceph_filestore", 0, 0);
+	add_file(td, td->o.filename ? : "cds_filestore", 0, 0);
 	td->o.nr_files = td->o.nr_files ? : 1;
     }
 
@@ -324,20 +345,20 @@ static int fio_ceph_filestore_setup(struct thread_data *td)
     return 0;
 
 cleanup:
-    fio_ceph_filestore_cleanup(td);
+    fio_cds_filestore_cleanup(td);
     return r;
 }
 
-static int fio_ceph_filestore_open(struct thread_data *td, struct fio_file *f)
+static int fio_cds_filestore_open(struct thread_data *td, struct fio_file *f)
 {
     return 0;
 }
 
-static void fio_ceph_filestore_io_u_free(struct thread_data *td,
+static void fio_cds_filestore_io_u_free(struct thread_data *td,
 					 struct io_u *io_u)
 {
-    struct fio_ceph_filestore_iou *o =
-	(struct fio_ceph_filestore_iou *) io_u->engine_data;
+    struct fio_cds_filestore_iou *o =
+	(struct fio_cds_filestore_iou *) io_u->engine_data;
 
     if (o) {
 	io_u->engine_data = NULL;
@@ -345,12 +366,12 @@ static void fio_ceph_filestore_io_u_free(struct thread_data *td,
     }
 }
 
-static int fio_ceph_filestore_io_u_init(struct thread_data *td,
+static int fio_cds_filestore_io_u_init(struct thread_data *td,
 					struct io_u *io_u)
 {
-    struct fio_ceph_filestore_iou *o;
+    struct fio_cds_filestore_iou *o;
 
-    o = (struct fio_ceph_filestore_iou *) malloc(sizeof(*o));
+    o = (struct fio_cds_filestore_iou *) malloc(sizeof(*o));
     o->io_complete = 0;
     o->io_u = io_u;
     io_u->engine_data = o;
@@ -367,19 +388,19 @@ extern "C" {
 
 	strcpy(ioengine->name, "ceph_cds_objectstore");
 	ioengine->version        = FIO_IOOPS_VERSION;
-	ioengine->setup          = fio_ceph_filestore_setup;
-	ioengine->init           = fio_ceph_filestore_init;
-	//ioengine->prep           = fio_ceph_filestore_prep;
-	ioengine->queue          = fio_ceph_filestore_queue;
-	//ioengine->cancel         = fio_ceph_filestore_cancel;
-	ioengine->getevents      = fio_ceph_filestore_getevents;
-	ioengine->event          = fio_ceph_filestore_event;
-	ioengine->cleanup        = fio_ceph_filestore_cleanup;
-	ioengine->open_file      = fio_ceph_filestore_open;
-	//ioengine->close_file     = fio_ceph_filestore_close;
-	ioengine->io_u_init      = fio_ceph_filestore_io_u_init;
-	ioengine->io_u_free      = fio_ceph_filestore_io_u_free;
+	ioengine->setup          = fio_cds_filestore_setup;
+	ioengine->init           = fio_cds_filestore_init;
+	//ioengine->prep           = fio_cds_filestore_prep;
+	ioengine->queue          = fio_cds_filestore_queue;
+	//ioengine->cancel         = fio_cds_filestore_cancel;
+	ioengine->getevents      = fio_cds_filestore_getevents;
+	ioengine->event          = fio_cds_filestore_event;
+	ioengine->cleanup        = fio_cds_filestore_cleanup;
+	ioengine->open_file      = fio_cds_filestore_open;
+	//ioengine->close_file     = fio_cds_filestore_close;
+	ioengine->io_u_init      = fio_cds_filestore_io_u_init;
+	ioengine->io_u_free      = fio_cds_filestore_io_u_free;
 	ioengine->options        = init_options();
-	ioengine->option_struct_size = sizeof(struct ceph_filestore_options);
+	ioengine->option_struct_size = sizeof(struct cds_filestore_options);
     }
 }
