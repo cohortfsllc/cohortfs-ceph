265d7cd820d044dee0ab0f3aa57516445170ab7b
Matt Benjamin <matt@linuxbox.com>
2014-06-02 18:26:46 -0400

    Add comment on offset cout hack.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/os/MemStore.cc
+++ src/os/MemStore.cc
@@ -1072,6 +1072,7 @@ int MemStore::_write(const coll_t &cid, const hobject_t& oid,
     c->object_hash[oid] = o;
   }
 
+  /* XXXX hack to see page inserts */
   if (offset == (16384 * 100)) {
     std::cout << "Stopping at " << offset << std::endl;
   }
