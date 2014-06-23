43b098e555bb2afadf04ea0c9c6b4d00be6a92ad
Matt Benjamin <matt@linuxbox.com>
2014-06-23 11:01:39 -0400

    Pull single-file CMake (via Adam) to Firefly.
    The goal is to build each source file only once.  We think
    we can restore multiple subdirs without losing that property,
    but havent verified.
    
    Builds to apparent completion.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/tools/ceph.cc
+++ src/tools/ceph.cc
@@ -183,7 +183,7 @@ static void parse_cmd_args(vector<const char*> &args,
 
 static int get_indata(const char *in_file, bufferlist &indata)
 {
-  int fd = VOID_TEMP_FAILURE_RETRY(::open(in_file, O_RDONLY));
+  int fd = TEMP_FAILURE_RETRY(::open(in_file, O_RDONLY));
   if (fd < 0) {
     int err = errno;
     derr << "error opening in_file '" << in_file << "': "
@@ -378,7 +378,7 @@ int main(int argc, const char **argv)
     if (out_file.empty() || out_file == "-") {
       err = outbl.write_fd(STDOUT_FILENO);
     } else {
-      int out_fd = VOID_TEMP_FAILURE_RETRY(::open(out_file.c_str(), O_WRONLY|O_CREAT|O_TRUNC, 0644));
+      int out_fd = TEMP_FAILURE_RETRY(::open(out_file.c_str(), O_WRONLY|O_CREAT|O_TRUNC, 0644));
       if (out_fd < 0) {
 	int ret = errno;
 	derr << " failed to create file '" << out_file << "': "
