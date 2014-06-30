linuxbox2/linuxbox-ceph

Permit AuthNoneAuthorizeHandler empty authorizer.
This removed an unexpected fault using XioMessenger for
Mon traffic, before Firefly merge.

Signed-off-by: Matt Benjamin <matt@linuxbox.com>

src/auth/none/AuthNoneAuthorizeHandler.cc
@@ -22,16 +22,17 @@ bool AuthNoneAuthorizeHandler::verify_authorizer(CephContext *cct, KeyStore *key
 						 EntityName& entity_name, uint64_t& global_id, AuthCapsInfo& caps_info, CryptoKey& session_key,
 uint64_t *auid)
 {
-  bufferlist::iterator iter = authorizer_data.begin();
-
-  try {
-    __u8 struct_v = 1;
-    ::decode(struct_v, iter);
-    ::decode(entity_name, iter);
-    ::decode(global_id, iter);
-  } catch (const buffer::error &err) {
-    ldout(cct, 0) << "AuthNoneAuthorizeHandle::verify_authorizer() failed to decode" << dendl;
-    return false;
+  if (authorizer_data.length() > 0) {
+    try {
+      __u8 struct_v = 1;
+      bufferlist::iterator iter = authorizer_data.begin();
+      ::decode(struct_v, iter);
+      ::decode(entity_name, iter);
+      ::decode(global_id, iter);
+    } catch (const buffer::error &err) {
+      ldout(cct, 0) << "AuthNoneAuthorizeHandle::verify_authorizer() failed to decode" << dendl;
+      return false;
+    }
   }
 
   caps_info.allow_all = true;

cohortfsllc/cohortfs-ceph

src/auth/none/AuthNoneAuthorizeHandler.cc
@@ -22,16 +22,17 @@ bool AuthNoneAuthorizeHandler::verify_authorizer(CephContext *cct, KeyStore *key
 						 EntityName& entity_name, uint64_t& global_id, AuthCapsInfo& caps_info, CryptoKey& session_key,
 uint64_t *auid)
 {
-  bufferlist::iterator iter = authorizer_data.begin();
-
-  try {
-    uint8_t struct_v = 1;
-    ::decode(struct_v, iter);
-    ::decode(entity_name, iter);
-    ::decode(global_id, iter);
-  } catch (const buffer::error &err) {
-    ldout(cct, 0) << "AuthNoneAuthorizeHandle::verify_authorizer() failed to decode" << dendl;
-    return false;
+  if (authorizer_data.length() > 0) {
+    try {
+      uint8_t struct_v = 1;
+      bufferlist::iterator iter = authorizer_data.begin();
+      ::decode(struct_v, iter);
+      ::decode(entity_name, iter);
+      ::decode(global_id, iter);
+    } catch (const buffer::error &err) {
+      ldout(cct, 0) << "AuthNoneAuthorizeHandle::verify_authorizer() failed to decode" << dendl;
+      return false;
+    }
   }
 
   caps_info.allow_all = true;