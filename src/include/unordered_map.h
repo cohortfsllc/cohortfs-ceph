d15af60332c912e2ab59f116c64dc2b0dd957490
Casey Bodley <casey@linuxbox.com>
2014-07-01 17:23:08 -0400

    c++11: unordered_map from std instead of std::tr1

--- src/include/unordered_map.h
+++ src/include/unordered_map.h
@@ -3,22 +3,10 @@
 
 #include <ciso646>
 
-#ifdef _LIBCPP_VERSION
-
 #include <unordered_map>
 
 namespace ceph {
   using std::unordered_map;
 }
-
-#else
-
-#include <tr1/unordered_map>
-
-namespace ceph {
-  using std::tr1::unordered_map;
-}
-
-#endif
 
 #endif
