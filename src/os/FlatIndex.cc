765bde7ad42df9fdc651b07665ab561b80cc5cf0
Adam C. Emerson <aemerson@linuxbox.com>
2014-07-15 16:54:40 -0400

    Do not garbagify filenames

--- src/os/FlatIndex.cc
+++ src/os/FlatIndex.cc
@@ -1,4 +1,4 @@
-// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*- 
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 /*
  * Ceph - scalable distributed file system
@@ -7,9 +7,9 @@
  *
  * This is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License version 2.1, as published by the Free Software 
+ * License version 2.1, as published by the Free Software
  * Foundation.  See file COPYING.
- * 
+ *
  */
 
 #if defined(__FreeBSD__)
@@ -155,6 +155,8 @@ static int append_oname(const hobject_t &oid, char *s, int len)
       *t++ = *i;
     i++;
   }
+
+  *t = '\0';
 
   int size = t - s;
 
