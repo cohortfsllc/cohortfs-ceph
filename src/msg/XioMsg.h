203a34ab0353fab75ff278102cdefa81f708fb44
Matt Benjamin <matt@linuxbox.com>
2014-08-04 18:04:40 -0400

    Minor cleanups in XioMsg.h.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/msg/XioMsg.h
+++ src/msg/XioMsg.h
@@ -346,8 +346,6 @@ public:
     put(1);
   }
 
-  XioConnection* get_xcon() { return xcon; }
-
   XioInSeq& get_seq() { return msg_seq; }
 
   void claim(int r) {}
@@ -371,9 +369,8 @@ struct XioRsp : public XioSubmit
   XioCompletionHook *xhook;
 public:
   XioRsp(XioConnection *_xcon, XioCompletionHook *_xhook)
-    : XioSubmit(XioSubmit::INCOMING_MSG_RELEASE, _xhook->get_xcon()),
-      xhook(_xhook->get())
-    {
+    : XioSubmit(XioSubmit::INCOMING_MSG_RELEASE, _xcon /* not xcon! */),
+      xhook(_xhook->get()) {
       // submit queue ref
       xcon->get();
     };
@@ -384,11 +381,10 @@ public:
 
   XioCompletionHook *get_xhook() { return xhook; }
 
-  void finalize()
-    {
-      xcon->put();
-      xhook->put();
-    }
+  void finalize() {
+    xcon->put();
+    xhook->put();
+  }
 };
 
 void print_xio_msg_hdr(const char *tag,  const XioMsgHdr &hdr,
