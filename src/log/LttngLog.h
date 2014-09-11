1d1dbe5c874e822290f671c2e8403c2fe0830cb0
Ali Maredia <ali@linuxbox.com>
2014-09-11 17:34:47 -0400

    Lttng Log Stream test working

--- src/log/LttngLog.h
+++ src/log/LttngLog.h
@@ -5,7 +5,7 @@
 
 #define TRACEPOINT_PROVIDER ceph
 #undef TRACEPOINT_INCLUDE
-#define TRACEPOINT_INCLUDE "/home/ali/ceph-local/src/log/LttngLog.h"
+#define TRACEPOINT_INCLUDE "log/LttngLog.h"
 
 #if !defined(CEPH_LOG_LTTNGLOG_H) || defined(TRACEPOINT_HEADER_MULTI_READ)
 #define CEPH_LOG_LTTNGLOG_H
@@ -14,14 +14,15 @@
 
 TRACEPOINT_EVENT( ceph,
        log_header,
-       TP_ARGS(int, entity_type, long, thread, int, pid, int, message_id, short, prio, short, subsys),
+       TP_ARGS(int, entity_type, const char *, entity_name, long, thread, int, pid, int, message_id, short, prio, short, subsys),
        TP_FIELDS(
 	     ctf_integer(int, entity_type, entity_type)
+	     ctf_string(entity_name, entity_name)
 	     ctf_integer(long, thread, thread)
 	     ctf_integer(int, pid, pid)
              ctf_integer(int, message_id, message_id)
-             ctf_integer(short, subsys, subsys)
              ctf_integer(short, prio, prio)
+             ctf_integer(short, subsys, subsys)
        )
 )
 
@@ -30,14 +31,15 @@ TRACEPOINT_LOGLEVEL(
        log_header,
        TRACE_INFO)
 
-TRACEPOINT_EVENT( ceph,
+TRACEPOINT_EVENT(ceph,
        log_integer,
-       TP_ARGS(int, identifier, char, origin_type, int, pid, int, message_id),
+       TP_ARGS(int, value, const char *, type, int, pid, int, message_id),
        TP_FIELDS(
 	     ctf_integer(int, value, value)
-	     ctf_integer(char, type, type)
+	     ctf_string(type, type)
 	     ctf_integer(int, pid, pid)
              ctf_integer(int, message_id, message_id)
+	)	
 )
 
 TRACEPOINT_LOGLEVEL(
@@ -45,13 +47,14 @@ TRACEPOINT_LOGLEVEL(
        log_integer,
        TRACE_INFO)
 
-TRACEPOINT_EVENT( ceph,
+TRACEPOINT_EVENT(ceph,
        log_string,
-       TP_ARGS( char, string, int, pid, int, message_id),
+       TP_ARGS(const char *, string, int, pid, int, message_id),
        TP_FIELDS(
-	     ctf_string(char, string, string)
+	     ctf_string(string, string)
 	     ctf_integer(int, pid, pid)
              ctf_integer(int, message_id, message_id)
+	)
 )
 
 TRACEPOINT_LOGLEVEL(
--- src/log/LttngStream.cc
+++ src/log/LttngStream.cc
@@ -3,11 +3,13 @@
 
 #include "LttngStream.h"
 #include "LttngLog.h"
+#include "Log.h"
 
-
-void lttng_stream::emit_header()
+void lttng_stream::emit_header(int entity_type, const char *entity_name,
+	 long thread, int pid, int message_id, short subsys, short prio)
 {
-  // TODO: emit header tracepoint with values from lttng_stream() constructor
+	// TODO: emit header tracepoint with values from lttng_stream() constructor
+	tracepoint(ceph, log_header, entity_type, entity_name, thread, pid, message_id, prio, subsys);
 }
 
 void lttng_stream::emit_integer(uint64_t val, enum int_type type)
