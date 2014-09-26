c07c81a0c6fcbc36b90f8f4856192a7c794f0fef
Casey Bodley <casey@linuxbox.com>
2014-09-26 10:56:28 -0400

    lttng: move StrmRet template to new header
    Signed-off-by: Casey Bodley <casey@linuxbox.com>

--- src/include/types.h
+++ src/include/types.h
@@ -80,6 +80,7 @@ extern "C" {
 
 #include "acconfig.h"
 
+#include "stream.h"
 #include "assert.h"
 
 // DARWIN compatibility
@@ -94,49 +95,6 @@ typedef long long off64_t;
 typedef off_t loff_t;
 typedef off_t off64_t;
 #endif
-
-/*
- * Templated stream output operators should support std::ostream and types
- * derived from std::ostream, as well as other types not derived from
- * std::ostream that still support operator<<.
- *
- * Types derived from std::ostream present a problem for templated operator<<,
- * because all default operator<< return std::ostream&. If the operator takes
- * stream type T and returns type T, it won't work for derived types like
- * std::stringstream; if the operator calls other stream operators that
- * return std::ostream, it can't return the result as a std::stringstream.
- *
- * We add a StrmRet template to solve this, for example:
- *
- * template<typename T>
- * inline typename StrmRet<T>::type operator<<(T &out, const foo &f) {
- *   return out << f.bar;
- * }
- *
- * Which will generate the following functions, with T=std::stringstream:
- *   std::ostream& operator<<(std::stringstream&, const foo &f)
- * and with T=lttng_stream:
- *   lttng_stream& operator<<(lttng_stream&, const foo &f)
- */
-#include <boost/type_traits.hpp>
-
-// default template keeps input type
-template<typename T, typename Base, bool IsDerived>
-struct BaseIfDerivedImpl { typedef T type; };
-
-// specialization replaces type with base
-template<typename T, typename Base>
-struct BaseIfDerivedImpl<T, Base, true> { typedef Base type; };
-
-// chooses type T, unless it inherits from Base
-template<typename T, typename Base>
-struct BaseIfDerived : public BaseIfDerivedImpl<T, Base,
-  boost::is_base_and_derived<Base, T>::value> {};
-
-// StrmRet<T>::type is set to T, or ostream if derived from ostream
-template<typename T>
-struct StrmRet : public BaseIfDerived<T, std::ostream> {};
-
 
 // -- io helpers --
 
