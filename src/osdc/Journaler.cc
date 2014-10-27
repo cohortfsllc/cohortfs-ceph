linuxbox2/linuxbox-ceph

journaler: consolidate journal entry buffers
the mds' journal writes to the objecter were sending thousands of buffer
segments, and eating up the accelio messenger's resources

Journaler::append_entry() now uses bufferlist::append() to consolidate
the small buffers into the bufferlist's page-sized append_buffer

Signed-off-by: Casey Bodley <casey@linuxbox.com>

src/osdc/Journaler.cc
@@ -448,7 +448,7 @@ uint64_t Journaler::append_entry(bufferlist& bl)
       bufferptr bp(write_pos - owp);
       bp.zero();
       assert(bp.length() >= 4);
-      write_buf.push_back(bp);
+      write_buf.append(bp.raw_c_str(), bp.raw_length());
       
       // now flush.
       flush();
@@ -459,9 +459,11 @@ uint64_t Journaler::append_entry(bufferlist& bl)
 	
   ldout(cct, 10) << "append_entry len " << bl.length() << " to " << write_pos << "~" << (bl.length() + sizeof(uint32_t)) << dendl;
   
-  // append
+  // append, but consolidate into page-sized buffers
   ::encode(s, write_buf);
-  write_buf.claim_append(bl);
+  std::list<buffer::ptr>::const_iterator p;
+  for (p = bl.buffers().begin(); p != bl.buffers().end(); ++p)
+    write_buf.append(p->c_str(), p->length());
   write_pos += sizeof(s) + s;
 
   // flush previous object?

cohortfsllc/cohortfs-ceph

src/osdc/Journaler.cc
@@ -441,7 +441,7 @@ uint64_t Journaler::append_entry(bufferlist& bl)
       bufferptr bp(write_pos - owp);
       bp.zero();
       assert(bp.length() >= 4);
-      write_buf.push_back(bp);
+      write_buf.append(bp.c_str(), bp.length());
 
       // now flush.
       flush();
@@ -452,9 +452,11 @@ uint64_t Journaler::append_entry(bufferlist& bl)
 
   ldout(cct, 10) << "append_entry len " << bl.length() << " to " << write_pos << "~" << (bl.length() + sizeof(uint32_t)) << dendl;
 
-  // append
+  // append, but consolidate into page-sized buffers
   ::encode(s, write_buf);
-  write_buf.claim_append(bl);
+  std::list<buffer::ptr>::const_iterator p;
+  for (p = bl.buffers().begin(); p != bl.buffers().end(); ++p)
+    write_buf.append(p->c_str(), p->length());
   write_pos += sizeof(s) + s;
 
   // flush previous object?