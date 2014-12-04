34478009284c3abf6bfcc55f8be0fbed07097c32
Ali Maredia <ali@linuxbox.com>
2014-12-04 13:39:01 -0500

    updated hacked together benchmark Matt together in 2 minutes

--- src/test/lttng_stream_test.cc
+++ src/test/lttng_stream_test.cc
@@ -7,7 +7,17 @@
 #include "common/debug.h"
 
 #define dout_subsys ceph_subsys_osd
+/*
+struct example_message {
+  example_message(int num_ints, int num_strings) 
+        : num_ints(num_ints), num_strings(num_strings) 
+        {
+        }
 
+  int num_ints;
+  int num_strings;  
+};
+*/
 
 int main(int argc, const char *argv[])
 {
@@ -20,13 +30,26 @@ int main(int argc, const char *argv[])
 
   common_init_finish(g_ceph_context);
 
+  thread_local string foo("", 1024); // stand-in for thread-local buffer
+
   int the_int = 4351352;
   string the_string = "cohortFS";
   auto t1 = std::chrono::high_resolution_clock::now();
   for(int i = 0; i < 1000000; i++) {
-    dout(0) << the_int << the_string << the_int
-      << the_string << the_int<< the_string 
-      << the_int << the_string << dendl; 
+#if 0
+    dout(1) << the_int << the_string << the_int
+      << the_string << dendl;
+#else
+    foo = "cohortFS";
+    foo += "cohortFS";
+    foo += "cohortFS";
+    foo += "cohortFS";
+    foo += the_int;
+    foo += the_int;
+    foo += the_int;
+    foo += the_int;
+    dout(1) << foo << dendl;
+#endif
   }
   auto t2 = std::chrono::high_resolution_clock::now();
   using std::chrono::duration_cast;
@@ -35,31 +58,5 @@ int main(int argc, const char *argv[])
   std::cout << "Duration is: " << duration.count() << " ms" << std::endl;
   return 0;
 }
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
