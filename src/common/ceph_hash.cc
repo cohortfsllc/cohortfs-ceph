b5e5d5b7d1b921bcb4be6bd8d58fa5bfc19c3a6b
Matt Benjamin <matt@linuxbox.com>
2015-02-08 15:18:14 -0500

    Redeclare ceph_str_hash as static inline
    This saves a function call, when the implementaiton is not
    inlined in ceph_hash.cc.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/common/ceph_hash.cc
+++ src/common/ceph_hash.cc
@@ -91,23 +91,6 @@ unsigned ceph_str_hash_linux(const char *str, unsigned length)
 	return hash;
 }
 
-#include "xxHash-r39/xxhash.h"
-
-uint64_t ceph_str_hash(int type, const char *s, unsigned len,
-    uint64_t seed = 667)
-{
-	switch (type) {
-	case CEPH_STR_HASH_XXHASH:
-	    return XXH64(s, len, seed);
-	case CEPH_STR_HASH_LINUX:
-	    return ceph_str_hash_linux(s, len);
-	case CEPH_STR_HASH_RJENKINS:
-	    return ceph_str_hash_rjenkins(s, len);
-	default:
-		return -1;
-	}
-}
-
 const char *ceph_str_hash_name(int type)
 {
 	switch (type) {
