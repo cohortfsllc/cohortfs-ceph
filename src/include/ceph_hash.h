65f017bf9031d215ca9cea775831946b42b5f74c
Matt Benjamin <matt@linuxbox.com>
2015-02-08 15:10:09 -0500

    Add xxhash64 (new BSD license)
    The XXH64() hash is the current top performer for speed+Q, for
    64-bit codes:
    
    Name     Speed on 64 bits    Speed on 32 bits
    XXH64       13.8 GB/s            1.9 GB/s
    XXH32        6.8 GB/s            6.0 GB/s
    
    (https://code.google.com/p/xxhash/)
    
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/include/ceph_hash.h
+++ src/include/ceph_hash.h
@@ -1,8 +1,9 @@
 #ifndef FS_CEPH_HASH_H
 #define FS_CEPH_HASH_H
 
-#define CEPH_STR_HASH_LINUX      0x1  /* linux dcache hash */
-#define CEPH_STR_HASH_RJENKINS   0x2  /* robert jenkins' */
+#define CEPH_STR_HASH_LINUX      0x001  /* linux dcache hash */
+#define CEPH_STR_HASH_RJENKINS   0x002  /* robert jenkins' */
+#define CEPH_STR_HASH_XXHASH     0x010
 
 extern unsigned ceph_str_hash_linux(const char *s, unsigned len);
 extern unsigned ceph_str_hash_rjenkins(const char *s, unsigned len);
