fe267f109d03cc305dcc1b73b77b56b7a34c7fca
Matt Benjamin <matt@linuxbox.com>
2015-02-08 15:18:14 -0500

    Redeclare ceph_str_hash as static inline
    This saves a function call, when the implementaiton is not
    inlined in ceph_hash.cc.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/include/ceph_hash.h
+++ src/include/ceph_hash.h
@@ -1,6 +1,8 @@
 #ifndef FS_CEPH_HASH_H
 #define FS_CEPH_HASH_H
 
+#include "xxHash-r39/xxhash.h"
+
 #define CEPH_STR_HASH_LINUX      0x001  /* linux dcache hash */
 #define CEPH_STR_HASH_RJENKINS   0x002  /* robert jenkins' */
 #define CEPH_STR_HASH_XXHASH     0x010
@@ -8,7 +10,21 @@
 extern unsigned ceph_str_hash_linux(const char *s, unsigned len);
 extern unsigned ceph_str_hash_rjenkins(const char *s, unsigned len);
 
-extern unsigned ceph_str_hash(int type, const char *s, unsigned len);
+static inline uint64_t
+ceph_str_hash(int type, const char *s, unsigned len, uint64_t seed = 667)
+{
+  switch (type) {
+  case CEPH_STR_HASH_XXHASH:
+    return XXH64(s, len, seed);
+  case CEPH_STR_HASH_LINUX:
+    return ceph_str_hash_linux(s, len);
+  case CEPH_STR_HASH_RJENKINS:
+    return ceph_str_hash_rjenkins(s, len);
+  default:
+    return -1;
+  }
+}
+
 extern const char *ceph_str_hash_name(int type);
 
 #endif
