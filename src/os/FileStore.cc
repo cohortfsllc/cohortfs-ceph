ab7a3ae3f30b1524151478636535ebfcc7a16f8b
Matt Benjamin <matt@cohortfs.com>
2015-02-16 15:48:48 -0500

    Add TreeX drain() method for close_collection().
    The caller can pass a closure to be called with each
    drained element.  This is used to return the sentinel
    LRU ref, so can lead to real destruction when that is
    the last outstanding ref.
    
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/os/FileStore.cc
+++ src/os/FileStore.cc
@@ -2383,6 +2383,9 @@ FileStore::FSObject* FileStore::get_object(FSCollection* fc,
   FDRef fd;
   FSObject* oh = nullptr;
 
+  if (fc->flags & Collection::FLAG_CLOSED) /* atomicity? */
+    return oh;
+
   uint64_t hk = XXH64(oid.oid.name.c_str(), oid.oid.name.size(), 667);
   std::tuple<uint64_t, const hobject_t&> k(hk, oid);
   Object::ObjCache::Latch lat;
@@ -3884,6 +3887,22 @@ ObjectStore::CollectionHandle FileStore::open_collection(const coll_t& cid)
 int FileStore::close_collection(CollectionHandle ch)
 {
   FSCollection* fc = static_cast<FSCollection*>(ch);
+  fc->flags |= Collection::FLAG_CLOSED;
+
+  class ObjUnref
+  {
+    FileStore* fs;
+  public:
+    ObjUnref(FileStore *_fs) : fs(_fs) {};
+
+    void operator()(Object::ObjCache::iterator it) const {
+      FSObject* oh = static_cast<FSObject*>(&(*it));
+      fs->obj_lru.unref(oh, cohort::lru::FLAG_NONE);
+    }
+  };
+
+  fc->obj_cache.drain(ObjUnref(this),
+		      Object::ObjCache::FLAG_LOCK);
   ::close(fc->fd);
   delete fc;
   return 0;
