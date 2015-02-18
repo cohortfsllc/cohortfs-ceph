ec85533933457c37d89e6a84df353bd6c198e641
Matt Benjamin <matt@cohortfs.com>
2015-02-18 11:20:26 -0500

    Sketch in Collection/FSCollection Lifecycle.
    Adds missing refcnt on (FS)Collection by (FSObject), defers
    deletion of open collections until all related objects are
    gone.
    
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/os/FileStore.cc
+++ src/os/FileStore.cc
@@ -2450,7 +2450,7 @@ retry:
     }
   }
   lat.mtx->unlock(); /* !LATCHED */
-out:
+ out:
   return oh;
 }
 
@@ -3934,10 +3934,13 @@ int FileStore::close_collection(CollectionHandle ch)
     }
   };
 
+  /* force cache drain, forces objects to evict */
   fc->obj_cache.drain(ObjUnref(this),
 		      Object::ObjCache::FLAG_LOCK);
-  ::close(fc->fd);
-  delete fc;
+
+  /* return initial ref */
+  fc->put();
+
   return 0;
 } /* close_collection */
 
