cd06de532dcbcb045d6e1d052f3f3d90b21b80dc
Casey Bodley <casey@cohortfs.com>
2015-02-19 14:56:51 -0500

    common: added cohort::ThreadPool and tests
    Signed-off-by: Casey Bodley <casey@cohortfs.com>

---
+++ src/common/ThreadPool.cc
@@ -0,0 +1,149 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2015 CohortFS, LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#include "ThreadPool.h"
+
+using namespace cohort;
+
+int ThreadPool::spawn(Job job)
+{
+  Worker *worker = new Worker();
+  worker->job = job;
+
+  // add the worker to the list
+  workers.push_back(*worker);
+
+  // spawn a thread to call worker_entry()
+  auto fn = [this, worker]() {
+    worker_entry(worker);
+  };
+  worker->thread = std::thread(fn);
+  return 0;
+}
+
+int ThreadPool::dispatch(Job job)
+{
+  // dispatch to the first idle worker
+  auto worker = idle.begin();
+  {
+    std::lock_guard<std::mutex> lock(worker->mutex);
+    assert(!worker->job);
+    worker->job = job;
+    worker->cond.notify_one();
+  }
+  idle.erase(worker);
+  return 0;
+}
+
+ThreadPool::Job ThreadPool::wait(Worker &worker)
+{
+  Job job;
+
+  std::unique_lock<std::mutex> pool_lock(mutex);
+  if (flags & FLAG_SHUTDOWN)
+    return job;
+
+  // take a job from the queue
+  if (!jobs.empty()) {
+    job.swap(jobs.front());
+    jobs.pop();
+    return job;
+  }
+
+  // enter the idle state
+  idle.push_back(worker);
+
+  std::unique_lock<std::mutex> worker_lock(worker.mutex);
+  assert(!worker.job);
+  pool_lock.unlock();
+
+  // wait for a signal from dispatch()
+  std::cv_status r;
+  if (flags & FLAG_SHUTDOWN)
+    r = std::cv_status::timeout;
+  else
+    r = worker.cond.wait_for(worker_lock, idle_timeout);
+
+  if (r == std::cv_status::timeout) {
+    worker_lock.unlock();
+    pool_lock.lock();
+    worker_lock.lock();
+
+    if (worker.job) // signal raced with locks
+      job.swap(worker.job);
+    else
+      idle.erase(idle.iterator_to(worker));
+    return job;
+  }
+
+  // signaled
+  job.swap(worker.job);
+  return job;
+}
+
+void ThreadPool::worker_entry(Worker *worker)
+{
+  Job job;
+  assert(worker->job); // from spawn()
+  job.swap(worker->job);
+
+  do {
+    job();
+    job = wait(*worker);
+  } while (job);
+
+  std::lock_guard<std::mutex> lock(mutex);
+  workers.erase(workers.iterator_to(*worker));
+  cond.notify_one();
+
+  dispose(std::move(worker->thread));
+  delete worker;
+}
+
+void ThreadPool::dispose(std::thread &&thread)
+{
+  if (graveyard.joinable()) // join previous thread
+    graveyard.join();
+
+  // put our thread in the graveyard so we can exit before its destruction
+  graveyard.swap(thread);
+}
+
+void ThreadPool::shutdown()
+{
+  std::unique_lock<std::mutex> pool_lock(mutex);
+  flags |= FLAG_SHUTDOWN;
+
+  // signal idle threads to shut down
+  for (auto &worker : idle) {
+    std::lock_guard<std::mutex> worker_lock(worker.mutex);
+    worker.cond.notify_one();
+  }
+
+  // finish jobs in queue
+  while (!jobs.empty()) {
+    jobs.front()();
+    jobs.pop();
+  }
+
+  // join workers
+  ceph::timespan wait = 1s;
+  while (!workers.empty()) {
+    cond.wait_for(pool_lock, wait);
+    wait = 5s;
+  }
+
+  if (graveyard.joinable())
+    graveyard.join();
+}
---
+++ src/test/test_threadpool.cc
@@ -0,0 +1,191 @@
+// -*- mode:C; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2015 CohortFS, LLC.
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License version 2, as published by the Free Software
+ * Foundation.	See file COPYING.
+ *
+ */
+
+#include "common/ThreadPool.h"
+#include "gtest/gtest.h"
+
+#include "common/ceph_argparse.h"
+#include "common/debug.h"
+#include "global/global_init.h"
+
+CephContext *cct;
+
+// test submit arguments
+namespace {
+int submit_a = 0;
+int submit_b = 0;
+int submit_c = 0;
+int submit_d = 0;
+void submit_job0() { submit_c = 3; }
+void submit_job1(int &x) { x = 4; }
+}
+TEST(ThreadPool, Submit)
+{
+  cohort::ThreadPool pool(cct, 1);
+  // submit with single lambda function
+  ASSERT_EQ(0, pool.submit([&]() { submit_a = 1; }));
+  // submit with lambda function and its argument
+  ASSERT_EQ(0, pool.submit([](int &x) { x = 2; }, std::ref(submit_b)));
+  // submit with standalone function
+  ASSERT_EQ(0, pool.submit(submit_job0));
+  // submit with standalone function and its argument
+  ASSERT_EQ(0, pool.submit(submit_job1, std::ref(submit_d)));
+  pool.shutdown();
+  ASSERT_EQ(1, submit_a);
+  ASSERT_EQ(2, submit_b);
+  ASSERT_EQ(3, submit_c);
+  ASSERT_EQ(4, submit_d);
+}
+
+// test that new threads are spawned when none are idle
+TEST(ThreadPool, Spawn)
+{
+  pthread_t a, b, c;
+
+  std::mutex mutex;
+  std::condition_variable cond;
+  bool block = true;
+
+  auto fn = [&](pthread_t &tid) {
+    std::unique_lock<std::mutex> lock(mutex);
+    while (block)
+      cond.wait(lock);
+    tid = pthread_self();
+  };
+
+  // queue up multiple jobs, spawning a new worker for each
+  cohort::ThreadPool pool(cct, 0);
+  ASSERT_EQ(0, pool.submit(fn, std::ref(a)));
+  ASSERT_EQ(0, pool.submit(fn, std::ref(b)));
+  ASSERT_EQ(0, pool.submit(fn, std::ref(c)));
+
+  // unblock the jobs
+  {
+    std::lock_guard<std::mutex> lock(mutex);
+    block = false;
+    cond.notify_all();
+  }
+  pool.shutdown();
+
+  // make sure each tid is different
+  ASSERT_TRUE(a != b);
+  ASSERT_TRUE(b != c);
+  ASSERT_TRUE(c != a);
+  ASSERT_TRUE(a != pthread_self());
+  ASSERT_TRUE(b != pthread_self());
+  ASSERT_TRUE(c != pthread_self());
+}
+
+// test that a single worker will take more jobs from the queue
+TEST(ThreadPool, Wait)
+{
+  pthread_t a, b, c;
+
+  std::mutex mutex;
+  std::condition_variable cond_block, cond_done;
+  bool block = true;
+  int jobs_done = 0;
+
+  auto fn = [&](pthread_t &tid) {
+    std::unique_lock<std::mutex> lock(mutex);
+    while (block)
+      cond_block.wait(lock);
+
+    tid = pthread_self();
+    ++jobs_done;
+    cond_done.notify_all();
+  };
+
+  // queue up multiple jobs for a single worker
+  cohort::ThreadPool pool(cct, 1);
+  ASSERT_EQ(0, pool.submit(fn, std::ref(a)));
+  ASSERT_EQ(0, pool.submit(fn, std::ref(b)));
+  ASSERT_EQ(0, pool.submit(fn, std::ref(c)));
+
+  // unblock the jobs
+  {
+    std::lock_guard<std::mutex> lock(mutex);
+    block = false;
+    cond_block.notify_all();
+  }
+  // wait for them to complete (so shutdown() thread doesn't handle any)
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+    while (jobs_done < 3)
+      cond_done.wait(lock);
+  }
+  pool.shutdown();
+
+  // make sure all tids are the same
+  ASSERT_EQ(a, b);
+  ASSERT_EQ(b, c);
+}
+
+// test that submit will dispatch jobs to idle workers
+TEST(ThreadPool, Dispatch)
+{
+  std::mutex mutex;
+  std::condition_variable cond;
+  bool block = true;
+
+  auto fn = [&]() {
+    std::lock_guard<std::mutex> lock(mutex);
+    block = false;
+    cond.notify_all();
+  };
+
+  // submit a job for a single worker
+  cohort::ThreadPool pool(cct, 1);
+  ASSERT_EQ(0, pool.submit(fn));
+
+  // wait for the job to complete (worker returns to idle state)
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+    while (block)
+      cond.wait(lock);
+  }
+  // submit another job
+  ASSERT_EQ(0, pool.submit([](){}));
+  pool.shutdown();
+}
+
+// test that threads exit after idle timeout
+TEST(ThreadPool, IdleTimeout)
+{
+  pthread_t a, b;
+
+  cohort::ThreadPool pool(cct, 1, 5ms);
+
+  ASSERT_EQ(0, pool.submit([&a]() { a = pthread_self(); }));
+
+  // sleep to make sure thread times out
+  std::this_thread::sleep_for(50ms);
+
+  ASSERT_EQ(0, pool.submit([&b]() { b = pthread_self(); }));
+  pool.shutdown();
+
+  ASSERT_TRUE(a != b);
+}
+
+int main(int argc, char *argv[])
+{
+  vector<const char*> args;
+  argv_to_vec(argc, (const char **)argv, args);
+
+  cct = global_init(NULL, args, CEPH_ENTITY_TYPE_CLIENT,
+                    CODE_ENVIRONMENT_UTILITY, 0);
+  common_init_finish(cct);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
