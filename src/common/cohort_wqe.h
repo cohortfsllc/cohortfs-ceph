7f496b5d082131b698a5e754289f085f512396ea
Matt Benjamin <matt@cohortfs.com>
2015-02-23 13:39:34 -0500

    Generalize WaitQueue* by outer lock type.
    Since waiters sleep only on wait queue entries (on themselves),
    the wait queue itself can be protected by any lock (e.g.,
    a Spinlock).
    
    Also, add an "ms" argument to wait_on--if supplied, the wait
    is a timed wait for ms milliseconds.
    
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/common/cohort_wqe.h
+++ src/common/cohort_wqe.h
@@ -18,8 +18,9 @@
 #include <mutex>
 #include <condition_variable>
 #include <atomic>
-#include <functional>
+#include <chrono>
 #include "common/likely.h"
+#include "include/Spinlock.h" /* has embedded cohort namespace */
 
 namespace cohort {
 
@@ -32,11 +33,11 @@ namespace cohort {
     std::condition_variable cv;
   };
 
-  template <typename T>
+  template <typename T, typename LK>
   class WaitQueueEntry {
   public:
     uint32_t flags;
-    WaitEntry lwe; /* left */
+    WaitEntry lwe;
     bi::list_member_hook<link_mode> q_hook;
     T data;
 
@@ -54,16 +55,17 @@ namespace cohort {
 
   }; /* WaitQueueEntry */
 
-  template <typename T>
+  template <typename T, typename LK>
   class WaitQueue {
   public:
-    typedef WaitQueueEntry<T> Entry;
-    typedef typename WaitQueueEntry<T>::Queue Queue;
+    typedef WaitQueueEntry<T,LK> Entry;
+    typedef typename WaitQueueEntry<T,LK>::Queue Queue;
     typedef typename Queue::iterator iterator;
 
     uint32_t flags;
     uint32_t waiters;
-    WaitEntry we;
+
+    LK lock;
     Queue queue;
 
     static constexpr uint32_t LFLAG_NONE = 0x0000;
@@ -73,36 +75,42 @@ namespace cohort {
     static constexpr uint32_t FLAG_SIGNAL = 0x0001;
     static constexpr uint32_t FLAG_LOCKED = 0x0002;
 
-    typedef std::unique_lock<std::mutex> unique_lock;
+    typedef std::unique_lock<LK> unique_lock;
+    typedef std::unique_lock<std::mutex> unique_we_lock;
 
     WaitQueue() : flags(LFLAG_NONE)
     {}
 
-    void wait_on(Entry& e) {
+    void wait_on(Entry& e, int ms=0) {
       /* set up unshared wait entry */
-      unique_lock lk(e.lwe.mtx);
+      unique_we_lock lk(e.lwe.mtx);
       e.flags = LFLAG_WAIT_SYNC;
       /* enqueue on shared waitq */
-      we.mtx.lock();
+      lock.lock();
       ++waiters;
       queue.push_back(e);
-      we.mtx.unlock(); /* release interlock */
-      e.lwe.cv.wait(lk);
+      lock.unlock(); /* release interlock */
+      if (ms) {
+	auto now = std::chrono::system_clock::now();
+	e.lwe.cv.wait_until(lk, now + std::chrono::milliseconds(ms));
+      }
+      else
+	e.lwe.cv.wait(lk);
     } /* wait_on */
 
     void dequeue(Entry& e, uint32_t flags) {
       iterator it = Queue::s_iterator_to(e);
       if (! (flags & FLAG_LOCKED))
-	we.mtx.lock();
+	lock.lock();
       --waiters;
       if (flags & FLAG_SIGNAL) {
-	unique_lock lk(e.lwe.mtx);
+	unique_we_lock lk(e.lwe.mtx);
 	e.flags &= ~LFLAG_WAIT_SYNC;
 	e.flags |= LFLAG_SYNC_DONE;
 	e.lwe.cv.notify_one();
       }
       if (! (flags & FLAG_LOCKED))
-	we.mtx.unlock();
+	lock.unlock();
     } /* dequeue */
   };
 
