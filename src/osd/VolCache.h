3e30f54c5907e12165e8e61810c5195a168fb156
Matt Benjamin <matt@cohortfs.com>
2015-03-09 21:45:43 -0400

    Introduce VolCache.h/cc.
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

---
+++ src/osd/VolCache.h
@@ -0,0 +1,27 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2015 CohortFS, LLC.
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.	See file COPYING.
+ *
+ */
+
+#ifndef VOL_CACHE_H
+#define VOL_CACHE_H
+
+#include <shared_mutex>
+#include <map>
+#include <memory>
+#include <mutex>
+#include "OSDVol.h"
+#include "common/Timer.h"
+#include "common/ceph_context.h"
+#include "os/ObjectStore.h"
+
+#endif /* VOL_CACHE_H */
