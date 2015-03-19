8d1c4fcec29dc5a7d426512f920a1d9aa0aeaebb
Matt Benjamin <matt@cohortfs.com>
2015-03-18 22:53:59 -0400

    Add single-lock guarded, unbounded (Ceph) queue.
    Add a workload basically identical to the single-lock
    queue in the pre-mailbox style.
    
    The unit test (test_queues) shows what happens when the queue
    naturally runs empty or nearly empty, and allows comparison
    with equivalent Vyukov MPMC bounded queues.
    
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/include/mpmc-slock-queue.h
+++ src/include/mpmc-slock-queue.h
@@ -37,6 +37,8 @@ namespace cohort {
       bi::list_member_hook<link_mode> q_hook;      
       T data;
 
+      Object(T data) : data(data) {}
+
       typedef bi::list<Object,
 		       bi::member_hook<Object,
 				       bi::list_member_hook<link_mode>,
@@ -50,7 +52,36 @@ namespace cohort {
     public:
       std::mutex mtx;
       std::condition_variable cv;
+      bool wait_sync;
+      typedef Object<T> object_t;
       typename Object<T>::Queue q;
+      typedef std::unique_lock<std::mutex> unique_lock;
+
+      Queue() : wait_sync(false) {};
+
+      bool enqueue(object_t* o) {
+	unique_lock lk(mtx);
+	q.push_back(*o);
+	if (wait_sync) {
+	  wait_sync = false;
+	  cv.notify_one();
+	}
+	return true;
+      }
+
+      object_t* dequeue() {
+	unique_lock lk(mtx);
+      retry:
+	if (q.size()) {
+	  object_t* o = &(q.front());
+	  q.pop_front();
+	  return o;
+	} else {
+	  wait_sync = true;
+	  cv.wait(lk);
+	}
+	goto retry;
+      }
      
     };
 
