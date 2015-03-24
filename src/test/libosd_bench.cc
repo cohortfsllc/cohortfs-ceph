1ef1f7544a03eabe367613de0c6894c59933728e
Casey Bodley <casey@cohortfs.com>
2015-03-24 12:05:58 -0400

    test: more for libosd_bench
    Signed-off-by: Casey Bodley <casey@cohortfs.com>

--- src/test/libosd_bench.cc
+++ src/test/libosd_bench.cc
@@ -2,6 +2,7 @@
 // vim: ts=8 sw=2 smarttab
 
 #include <iostream>
+#include <thread>
 
 #include "libosd/ceph_osd.h"
 #include "common/ceph_argparse.h"
@@ -18,6 +19,46 @@ static void usage()
   generic_server_usage();
 }
 
+void benchmark_thread(struct libosd *osd, uint8_t *volume, int count)
+{
+  // do 'count' synchronous reads on a nonexistent object
+  char buffer[16];
+  for (int i = 0; i < count; i++) {
+    int r = osd->read("foo", volume, 0, sizeof(buffer), buffer,
+                      LIBOSD_READ_FLAGS_NONE, nullptr, nullptr);
+    if (r != 0)
+      std::cerr << "libosd_read() failed with " << r << std::endl;
+  }
+}
+
+void benchmark(struct libosd *osd, uint8_t *uuid, int nthreads, int count)
+{
+  auto fn = [osd, uuid, count]() {
+    benchmark_thread(osd, uuid, count);
+  };
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+  std::cout << "time started " << t1 << std::endl;
+
+  // start threads
+  std::vector<std::thread> threads;
+  for (int i = 0; i < nthreads; i++)
+    threads.emplace_back(fn);
+
+  // join threads
+  for (auto &t : threads)
+    t.join();
+
+  auto t2 = std::chrono::high_resolution_clock::now();
+  std::cout << "time finished " << t2 << std::endl;
+
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+
+  std::cout << "count " << count << std::endl;
+  std::cout << "duration " << duration.count() << "ms" << std::endl;
+  std::cout << "iops " << 1000000LL * count / duration.count() << std::endl;
+}
+
 int main(int argc, const char *argv[])
 {
   const struct libosd_init_args init_args {
@@ -32,6 +73,7 @@ int main(int argc, const char *argv[])
 
   // default values
   int threads = 1;
+  int count = 1;
   std::string volume;
 
   // command-line arguments
@@ -46,26 +88,35 @@ int main(int argc, const char *argv[])
 
     if (ceph_argparse_witharg(args, i, &val, "--threads", (char*)NULL)) {
       threads = atoi(val.c_str());
+    } else if (ceph_argparse_witharg(args, i, &val, "--count", (char*)NULL)) {
+      count = atoi(val.c_str());
     } else if (ceph_argparse_witharg(args, i, &val, "--volume", (char*)NULL)) {
       volume = val;
-    } else {
-      std::cerr << "Error: can't understand argument: " << *i << std::endl;
-      usage();
-    }
+    } else
+      ++i;
   }
 
   if (threads < 1 || threads > 256) {
-    std::cerr << "Invalid thread count " << threads << std::endl;
+    std::cerr << "Invalid value for threads " << threads << std::endl;
+    usage();
+    return EXIT_FAILURE;
+  }
+  if (count < 1) {
+    std::cerr << "Invalid value for count " << count << std::endl;
+    usage();
     return EXIT_FAILURE;
   }
   if (volume.empty()) {
     std::cerr << "Missing argument --volume" << std::endl;
+    usage();
     return EXIT_FAILURE;
   }
 
   std::cout << "threads " << threads << std::endl;
+  std::cout << "count " << count << std::endl;
   std::cout << "volume " << volume << std::endl;
 
+  // start osd
   struct libosd* osd = libosd_init(&init_args);
   if (osd == nullptr) {
     std::cerr << "libosd_init() failed" << std::endl;
@@ -79,6 +130,9 @@ int main(int argc, const char *argv[])
     return EXIT_FAILURE;
   }
 
+  benchmark(osd, uuid, threads, count);
+
+  // shutdown and cleanup
   osd->shutdown();
   osd->join();
   libosd_cleanup(osd);
