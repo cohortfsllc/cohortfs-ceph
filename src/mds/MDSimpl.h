50c8b5dff4acd8e04ac984e47d97e08eb1425245
Marcus Watts <mdw@linuxbox.com>
2015-04-24 16:28:36 -0400

    Forgot to push this piece of libmds.  This is part of the mds split between interface and implementation.
    Signed-off-by: Marcus Watts <mdw@cohortfs.com>

---
+++ src/mds/MDSimpl.h
@@ -0,0 +1,41 @@
+#ifndef _MDSINT_H
+#define _MDSINT_H 1
+#define IN_TREE_BUILD 1
+#include "msg/Dispatcher.h"
+#include <mds/MDS.h>
+class MDSimpl : public MDS, Dispatcher {
+public:
+    string name;
+    int whoami;
+    Messenger *messenger;
+    MonClient *monc;
+    MDSMap *mdsmap;
+    Objecter *objecter;
+    Finisher *finisher;
+protected:
+    int last_state, state, want_state;
+    Context *createwaitingforosd;
+    ceph_tid_t last_tid;
+public:
+    int get_state() { return state; }
+    int get_want_state() { return want_state; }
+    void request_state(int s);
+    ceph_tid_t issue_tid() { return ++last_tid; }
+    version_t beacon_last_seq;
+    bool was_laggy;
+    uint64_t beacon_sender;
+    MDSimpl(const std::string &n, Messenger *m, MonClient *mc);
+    ~MDSimpl();
+    int get_nodeid() { return whoami; }
+    MDSMap *get_mds_map() { return mdsmap; }
+    int init(int wanted_state = MDSMap::STATE_BOOT);
+    void beacon_start();
+    void beacon_send();
+    // void handle_mds_beacon(MMDSBeacon *m);
+    void handle_signal(int signum);
+    bool ms_dispatch(Message *m);
+    bool ms_handle_reset(Connection *con);
+    void ms_handle_remote_reset(Connection *con);
+    bool shutdown();
+};
+#endif /* _MDSINT_H */
