d5b5063df61322cc263ff3e8607c53ea233adf0b
Matt Benjamin <matt@cohortfs.com>
2015-04-24 17:44:10 -0400

    zfstore:  add initial files and cmake build
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

---
+++ src/os/ZFStore.h
@@ -0,0 +1,18 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2015 CohortFS, LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#ifndef COHORT_ZFSTORE_H
+#define COHORT_ZFSTORE_H
+
+#endif /* COHORT_ZFSTORE_H */
