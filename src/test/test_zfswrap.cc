f1eca05f85c5445e6694c2535894a47b52ef4ac8
Matt Benjamin <matt@cohortfs.com>
2015-05-14 22:42:48 -0400

    zfswrap: create and destroy a filesystem on zpool1
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/test/test_zfswrap.cc
+++ src/test/test_zfswrap.cc
@@ -85,6 +85,27 @@ TEST(ZFSWRAP, ZPLIST)
   ASSERT_EQ(err, 0);
 }
 
+TEST(ZFSWRAP, ZFS1)
+{
+  // create a filesystem on zp1
+  int err;
+  const char* lzw_err;
+  const int ZFS_TYPE_FILESYSTEM = 0x1;
+
+  err = lzfw_dataset_create(zhd, "zp1/zfs1", ZFS_TYPE_FILESYSTEM,
+			    &lzw_err);
+  ASSERT_EQ(err, 0);
+}
+
+TEST(ZFSWRAP, ZFSDESTROY1)
+{
+  int err;
+  const char* lzw_err;
+
+  err = lzfw_dataset_destroy(zhd, "zp1/zfs1", &lzw_err);
+  ASSERT_EQ(err, 0);
+}
+
 /* TODO: finish */
 
 TEST(ZFSWRAP, ZPDESTROY1)
