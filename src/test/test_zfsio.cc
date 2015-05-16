c583ba7b060f48b644ada3258434c667a328b993
Matt Benjamin <matt@cohortfs.com>
2015-05-16 13:00:44 -0400

    zfsio: framework for testing vector and async io
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

---
+++ src/test/test_zfsio.cc
@@ -0,0 +1,174 @@
+// -*- mode:C; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2015 CohortFS, LLC.
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License version 2, as published by the Free Software
+ * Foundation.	See file COPYING.
+ *
+ */
+
+#include <sys/types.h>
+#include <iostream>
+#include <vector>
+#include "gtest/gtest.h"
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/exception.hpp>
+#include <boost/format.hpp>
+#include "common/ceph_argparse.h"
+#include "common/debug.h"
+#include "global/global_init.h"
+
+extern "C" {
+#include <libzfswrap.h>
+}
+
+namespace bf = boost::filesystem;
+
+namespace {
+
+  bool create = false; // will pre-destroy
+  bool destroy = false;
+
+  bf::path vdevs("/opt/zpools");
+  bf::path vdev1(vdevs);
+
+  lzfw_handle_t* zhd; /* zfswrap handle */
+  lzfw_vfs_t* zhfs; /* dataset handle */
+  lzfw_vnode_t* root_vnode = nullptr;
+  inogen_t root_ino = {0, 0};
+  creden_t cred = {0, 0};
+
+  struct ZFSObject
+  {
+    std::string leaf_name;
+    inogen_t ino;
+    lzfw_vnode_t* vnode;
+
+  ZFSObject(std::string n) : leaf_name(std::move(n)), ino{0, 0},
+      vnode(nullptr)
+    {}
+  };
+
+  std::vector<ZFSObject> zfs1_objs;
+
+} /* namespace */
+
+TEST(ZFSIO, INIT)
+{
+  int err;
+  const char* lzw_err;
+
+  zhd = lzfw_init();
+  ASSERT_NE(zhd, nullptr);
+
+  vdev1 /= "zd2";
+
+  if (create || !is_regular_file(vdev1)) {
+    { // create backing
+      std::string cmd = "dd if=/dev/zero of=";
+      cmd += vdev1.c_str();
+      cmd += " bs=1M count=500";
+      std::cout << cmd.c_str() << std::endl;
+      system(cmd.c_str());
+    }
+    { // create pool
+      const char* v[1]; // an array of devices
+      v[0] = vdev1.c_str();
+      err = lzfw_zpool_create(zhd, "zp2", "" /* type */, v, 1,
+			      &lzw_err);
+      ASSERT_EQ(err, 0);
+    }
+    { // create dataset
+      const int ZFS_TYPE_FILESYSTEM = 0x1;
+      // XXX spa code edits strings in-place--can't pass const!
+      char* fs = strdup("zp2/zf2");
+      err = lzfw_dataset_create(zhd, fs, ZFS_TYPE_FILESYSTEM,
+				&lzw_err);
+      ASSERT_EQ(err, 0);
+      free(fs);
+    }
+  } /* create */
+
+  // mount fs
+  zhfs = lzfw_mount("zp2", "/zf2", "" /* XXX "mount options" */);
+  ASSERT_NE(zhfs, nullptr);
+
+  // get root ino
+  err = lzfw_getroot(zhfs, &root_ino);
+  ASSERT_EQ(err, 0);
+
+  // open root vnode
+  err = lzfw_opendir(zhfs, &cred, root_ino, &root_vnode);
+  ASSERT_EQ(err, 0);
+  ASSERT_NE(root_vnode, nullptr);
+}
+
+/* TODO: finish */
+
+TEST(ZFSIO, SHUTDOWN)
+{
+  int err;
+  const char* lzw_err;
+
+  // close root vnode
+  err = lzfw_closedir(zhfs, &cred, root_vnode);
+  root_vnode = nullptr;
+  ASSERT_EQ(err, 0);
+
+  // release fs
+  err = lzfw_umount(zhfs, true /* force */);
+  zhfs = nullptr;
+  ASSERT_EQ(err, 0);
+
+  // cond destroy everything
+  if (destroy) {
+    { // destroy fs
+      char* fs = strdup("zp2/zf2");
+      err = lzfw_dataset_destroy(zhd, fs, &lzw_err);
+      ASSERT_EQ(err, 0);
+      free(fs);
+    }
+    { // destroy pool
+      err = lzfw_zpool_destroy(zhd, "zp1", true /* force */, &lzw_err);
+      ASSERT_EQ(err, 0);
+      remove(vdev1);
+    }
+  }
+
+  // release library
+  lzfw_exit(zhd);
+  zhd = nullptr;
+}
+
+int main(int argc, char *argv[])
+{
+  vector<const char*> args;
+  argv_to_vec(argc, (const char **)argv, args);
+
+  CephContext *cct =
+    global_init(NULL, args, CEPH_ENTITY_TYPE_CLIENT,
+		CODE_ENVIRONMENT_UTILITY, 0);
+
+  string val;
+  vector<const char*>::iterator i = args.begin();
+  while (i != args.end()) {
+    if (ceph_argparse_double_dash(args, i))
+      break;
+    if (ceph_argparse_flag(args, &i, "--create", (char*)NULL)) {
+      create = true;
+      continue;
+    }
+    if (ceph_argparse_flag(args, &i, "--destroy", (char*)NULL)) {
+      destroy = true;
+      continue;
+    }
+  } /* while(args) */
+  common_init_finish(cct);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
