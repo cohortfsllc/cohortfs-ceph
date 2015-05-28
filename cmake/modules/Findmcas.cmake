2aaae537be43af27ad59b2896d8f9a91894a037d
Casey Bodley <casey@cohortfs.com>
2015-05-28 17:06:12 -0400

    cmake: add mcas module and dependency for libmds
    Signed-off-by: Casey Bodley <casey@cohortfs.com>

---
+++ cmake/modules/Findmcas.cmake
@@ -0,0 +1,20 @@
+# - Find mcas
+#
+#  MCAS_INCLUDE_DIRS   - where to find mcas/mcas.h, etc.
+#  MCAS_LIBRARIES      - List of libraries when using mcas.
+#  MCAS_FOUND          - True if mcas found.
+
+find_path(MCAS_INCLUDE_DIR NAMES mcas/mcas.h)
+mark_as_advanced(MCAS_INCLUDE_DIR)
+
+find_library(MCAS_LIBRARY NAMES mcas)
+mark_as_advanced(MCAS_LIBRARY)
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(MCAS
+                                  REQUIRED_VARS MCAS_LIBRARY MCAS_INCLUDE_DIR)
+
+if(MCAS_FOUND)
+  set(MCAS_LIBRARIES ${MCAS_LIBRARY})
+  set(MCAS_INCLUDE_DIRS ${MCAS_INCLUDE_DIR})
+endif()
