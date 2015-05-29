e478423f0e3305919d47dd03ef79c247d8644203
Matt Benjamin <matt@cohortfs.com>
2015-05-29 14:38:18 -0400

    zfstore: update and parse new zpool config syntax
    Verbose Boost range-based string splitting--evidently, not slow
    like boost::tokenizer.
    
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/test/test_zfsio.cc
+++ src/test/test_zfsio.cc
@@ -16,11 +16,15 @@
 #include <sys/uio.h>
 #include <iostream>
 #include <vector>
+#include <map>
 #include <random>
+#include <tuple>
 #include "xxHash-r39/xxhash.h"
 #include "gtest/gtest.h"
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/exception.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/split.hpp>
 #include <boost/format.hpp>
 #include "common/ceph_argparse.h"
 #include "common/debug.h"
@@ -130,6 +134,58 @@ namespace {
     }
   }; /* ZPageSet */
 
+// name, type (""), list of device paths
+typedef std::tuple<std::string, std::string, std::vector<std::string>>
+  zp_desc_type;
+
+typedef std::map<std::string, zp_desc_type> zp_desc_map;
+
+typedef std::vector<boost::iterator_range<std::string::iterator> >
+fv_type;
+
+ void print_zp_desc_map(const zp_desc_map& zpm)
+ {
+   using std::get;
+   for (auto& it : zpm) {
+     const std::string& pool_name = it.first;
+     std::cout << "zpool: " << pool_name
+	       << " type: " << get<1>(it.second)
+	       << " paths:";
+     const vector<std::string>& devs = get<2>(it.second);
+     for (auto& dev_iter : devs) {
+       std::cout << " " << dev_iter;
+     }
+     std::cout << std::endl;
+   }
+ }
+
+ int parse_zp_desc(std::string desc, zp_desc_map& zpm) {
+
+   namespace ba = boost::algorithm;
+
+   zpm.clear();
+
+   fv_type fv_outer;
+   ba::split(fv_outer, desc, ba::is_any_of(";"));
+   for (auto& oit : fv_outer) {
+     fv_type fv_inner;
+     ba::split(fv_inner, oit, ba::is_any_of(","));
+     if (fv_inner.size() >= 3) {
+       vector<std::string> dev_paths;
+       std::string name{fv_inner[1].begin(), fv_inner[1].end()};
+       std::string type{fv_inner[0].begin(), fv_inner[0].end()};
+       fv_type::iterator dev_iter = fv_inner.begin() + 2;
+       for (; dev_iter != fv_inner.end(); ++dev_iter) {
+	 dev_paths.push_back(std::string{dev_iter->begin(), dev_iter->end()});
+       }
+       zpm.insert(zp_desc_map::value_type{
+	   name, zp_desc_type{name, type, dev_paths}});
+     }
+   }
+
+   return zpm.size();;
+ }
+
 } /* namespace */
 
 TEST(ZFSIO, INIT)
@@ -174,6 +230,12 @@ TEST(ZFSIO, INIT)
     // seed rng
     rng.seed(1337);
   } /* create */
+
+  // validate zpool /* XXX in progress, currently, lists all */
+  err = lzfw_zpool_list(zhd, NULL /* props */, &lzw_err);
+  if (err) {
+    ASSERT_EQ(nullptr, lzw_err);
+  }
 
   // mount fs
   zhfs = lzfw_mount("zp2", "/zf2", "" /* XXX "mount options" */);
@@ -243,6 +305,23 @@ TEST(ZFSIO, CLOSE1)
     err = lzfw_close(zhfs, &cred, o.vnode, O_RDWR|O_CREAT);
     ASSERT_EQ(err, 0);
   }
+}
+
+TEST(ZFSIO, POOLDEV_SYNTAX)
+{
+  int sz;
+  zp_desc_map zpm;
+
+  // parse the default zpool device map rep
+  std::string zfstore_zpool_devices = "default,zp_default,/opt/zpools/zp_default";
+  sz = parse_zp_desc(zfstore_zpool_devices, zpm);
+  print_zp_desc_map(zpm);
+
+  // parse a map rep with multiple pool descriptions
+  zfstore_zpool_devices =
+    "default,zp_one,/opt/zpools/zp_one;default,zp_two,/opt/zpools/zp_two";
+  sz = parse_zp_desc(zfstore_zpool_devices, zpm);
+  print_zp_desc_map(zpm);
 }
 
 TEST(ZFSIO, SHUTDOWN)
