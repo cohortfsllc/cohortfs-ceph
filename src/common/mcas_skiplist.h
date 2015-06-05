001c26f4cf05368de65c82e49766f0b1fee63d50
Casey Bodley <casey@cohortfs.com>
2015-06-05 16:24:24 -0400

    mcas: move skiplist_base functions to .cc file
    Signed-off-by: Casey Bodley <casey@cohortfs.com>

--- src/common/mcas_skiplist.h
+++ src/common/mcas_skiplist.h
@@ -91,75 +91,11 @@ class skiplist_base {
     assert(r == 0);
   }
 
-  skip_stats* get_mythread_stats()
-  {
-    thread_stats *a = static_cast<thread_stats*>(pthread_getspecific(tls_key));
-    if (a && a->deleted) // probably "can't happen"
-      return 0;
+  skip_stats* get_mythread_stats();
+  void get_stats(skip_stats *s);
 
-    if (!a) {
-      a = new thread_stats();
-      memset(&a->stats, 0, sizeof(a->stats));
-      memset(&a->stats_last, 0, sizeof(a->stats_last));
-      a->deleted = 0;
-      a->skiplist = this;
-      {
-        std::lock_guard<std::mutex> lock(mutex);
-        a->next = thread_list;
-        thread_list = a;
-      }
-      int r = pthread_setspecific(tls_key, a);
-      assert(r == 0);
-    }
-    return &a->stats;
-  }
-
-  static void sumup(thread_stats *p)
-  {
-    skip_stats &stats = p->skiplist->stats;
-    skip_stats &s = p->stats;
-    skip_stats &l = p->stats_last;
-
-    stats.gets += s.gets - l.gets;
-    stats.gets_created += s.gets_created - l.gets_created;
-    stats.gets_stillborn += s.gets_stillborn - l.gets_stillborn;
-    stats.gets_existing += s.gets_existing - l.gets_existing;
-    stats.puts += s.puts - l.puts;
-    stats.puts_last += s.puts_last - l.puts_last;
-    if (stats.maxsize < s.maxsize)
-      stats.maxsize = s.maxsize;
-    l = s;
-  }
-
-  static void sumup_and_free(void *arg)
-  {
-    thread_stats *p = static_cast<thread_stats*>(arg);
-    thread_stats** pp, *q;
-
-    skiplist_base *skiplist = p->skiplist;
-    if (!p->deleted) {
-      std::lock_guard<std::mutex> lock(skiplist->mutex);
-      sumup(p);
-      for (pp = &skiplist->thread_list; (q = *pp); pp = &q->next) {
-        if (q == p) {
-          *pp = p->next;
-          break;
-        }
-      }
-      assert(q);
-    }
-    delete p;
-  }
-
-  void get_stats(skip_stats *s)
-  {
-    std::lock_guard<std::mutex> lock(mutex);
-    for (thread_stats *p = thread_list; p; p = p->next)
-      sumup(p);
-
-    *s = stats;
-    s->size = size;
-  }
+  static void sumup_and_free(void *arg); // destructor for tls
+  static void sumup(thread_stats *p);
 };
 
 template <typename T>
