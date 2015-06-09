a6f2c65d34f8c7bf11906869e6c6c86ec881819b
Casey Bodley <casey@cohortfs.com>
2015-06-09 15:52:34 -0400

    test: add mcas_skiplist test for reaper
    Signed-off-by: Casey Bodley <casey@cohortfs.com>

--- src/test/test_mcas_skiplist.cc
+++ src/test/test_mcas_skiplist.cc
@@ -3,6 +3,8 @@
 
 using namespace cohort::mcas;
 
+gc_global gc;
+
 struct test_object : public skiplist<test_object>::object {
   int key;
   test_object(int key) : key(key) {}
@@ -23,14 +25,19 @@ void intrusive_ptr_release(test_object *p) { p->put(); }
 
 TEST(Skiplist, Get)
 {
-  gc_global gc;
   skiplist<test_object> skip(gc, test_object::cmp, "test");
   skip_stats stats;
 
   auto obj = skip.get(test_object(5));
-  ASSERT_TRUE(obj != nullptr);
+  ASSERT_EQ(nullptr, obj);
   skip.get_stats(&stats);
   ASSERT_EQ(1, stats.gets);
+  ASSERT_EQ(1, stats.gets_miss);
+
+  obj = skip.get_or_create(test_object(5));
+  ASSERT_TRUE(obj != nullptr);
+  skip.get_stats(&stats);
+  ASSERT_EQ(2, stats.gets);
   ASSERT_EQ(1, stats.gets_created);
   ASSERT_EQ(0, stats.gets_existing);
   ASSERT_EQ(0, stats.puts);
@@ -42,11 +49,31 @@ TEST(Skiplist, Get)
 
   obj = skip.get(test_object(5));
   skip.get_stats(&stats);
-  ASSERT_EQ(2, stats.gets);
+  ASSERT_EQ(3, stats.gets);
   ASSERT_EQ(1, stats.gets_existing);
 
   obj.reset();
   skip.get_stats(&stats);
   ASSERT_EQ(2, stats.puts);
   ASSERT_EQ(2, stats.puts_last);
+}
+
+TEST(Skiplist, Reap)
+{
+  skiplist<test_object> skip(gc, test_object::cmp, "test", 0, 1);
+
+  // hold a reference to the first object
+  auto obj = skip.get_or_create(test_object(1));
+  // drop the reference on the rest
+  skip.get_or_create(test_object(2));
+  skip.get_or_create(test_object(3));
+  skip.get_or_create(test_object(4));
+
+  // let the reaper thread run; it shouldn't sleep
+  std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+  skip_stats stats;
+  skip.get_stats(&stats);
+  ASSERT_EQ(3, stats.reaped);
+  ASSERT_EQ(1, stats.size);
 }
