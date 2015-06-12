30674f16e73269b8a61d1b356827075d6b2dd906
Marcus Watts <mdw@cohortfs.com>
2015-06-12 02:24:02 -0400

    Case matters!  Without this fix, failing to find mcas does not result in failing to cmake.
    Signed-off-by: Marcus Watts <mdw@cohortfs.com>

--- cmake/modules/Findmcas.cmake
+++ cmake/modules/Findmcas.cmake
@@ -11,9 +11,12 @@ find_library(MCAS_LIBRARY NAMES mcas)
 mark_as_advanced(MCAS_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(MCAS
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(mcas
                                   REQUIRED_VARS MCAS_LIBRARY MCAS_INCLUDE_DIR)
 
+if(mcas_FOUND)
+  set(MCAS_FOUND 1)
+endif()
 if(MCAS_FOUND)
   set(MCAS_LIBRARIES ${MCAS_LIBRARY})
   set(MCAS_INCLUDE_DIRS ${MCAS_INCLUDE_DIR})
